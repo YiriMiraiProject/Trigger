d7ce9570c5b399284087ec484be92d805c25e479
忘忧北萱草 <wybxc@qq.com>
2021-08-16 21:51:06 +0800

    v0.2.0

--- mirai_extensions/trigger/__init__.py
+++ mirai_extensions/trigger/__init__.py
@@ -2,6 +2,10 @@
 """
 事件触发器：提供更多处理事件的方式。
 """
+
+__version__ = '0.2.0'
+__author__ = '忘忧北萱草'
+
 from mirai_extensions.trigger.handler import HandlerControl
 from mirai_extensions.trigger.interrupt import InterruptControl
 from mirai_extensions.trigger.message import (
